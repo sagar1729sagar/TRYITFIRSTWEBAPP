e60e26669e4a190062ad539f0c0bb53441f70e9f
sagar <sagar1729sagar@gmail.com>
2019-05-20 15:22:33 +0530

    Splash screen coded

--- app/src/main/java/com/ssapps/tryitfirstwebapp/MainActivity.java
+++ app/src/main/java/com/ssapps/tryitfirstwebapp/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-               
+
                 startActivity(new Intent(MainActivity.this,WebViewActivity.class));
 
             }
