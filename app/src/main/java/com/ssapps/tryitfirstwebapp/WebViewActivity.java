d95e22da4f13ef328cc1b8e6f50bbb7cb83906cc
sagar <sagar1729sagar@gmail.com>
2019-05-20 17:34:21 +0530

    Webview inplemented

--- app/src/main/java/com/ssapps/tryitfirstwebapp/WebViewActivity.java
+++ app/src/main/java/com/ssapps/tryitfirstwebapp/WebViewActivity.java
@@ -1,13 +1,53 @@
 package com.ssapps.tryitfirstwebapp;
 
+import android.graphics.Bitmap;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
 
+    private SwipeRefreshLayout refreshLayout;
+    private WebView webView;
+    private static String url = "https://dev2.tryitfirst.online";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+
+        refreshLayout = (SwipeRefreshLayout)findViewById(R.id.refresh_layout);
+        webView = (WebView)findViewById(R.id.webview);
+        refreshLayout.setRefreshing(true);
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                refreshLayout.setRefreshing(true);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                refreshLayout.setRefreshing(false);
+            }
+        });
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshLayout.setRefreshing(true);
+                webView.reload();
+            }
+        });
+
+
+
     }
 }
